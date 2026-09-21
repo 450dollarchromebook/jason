fda393d46790b146a00aa9070324d3481a975ccf
450dollarchromebook <111309873+450dollarchromebook@users.noreply.github.com>
2026-09-21 16:20:23 -0700

    Update you.js

--- you.js
+++ you.js
@@ -21,7 +21,7 @@ function musicPlay() {
 	document.removeEventListener('click', musicPlay);
 }
 
-var faudio = new Audio('youare.mp3')
+var faudio = new Audio('jason.mp3')
 
 faudio.addEventListener('timeupdate', function() {
 	console.log('TimeUpdate invoked.');
